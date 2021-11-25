7d692730c293ba7d4e0702654b64064e9c1c6452
Guilherme <guipereira1586@outlook.com>
2021-11-24 21:59:08 -0300

    Card config

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,17 @@
 <template>
+<v-app-bar
+      :collapse="!collapseOnScroll"
+      :collapse-on-scroll="collapseOnScroll"
+      absolute
+      color="deep-purple accent-4"
+      dark
+      scroll-target="#scrolling-techniques-6"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Collapsing Bar</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
 </template>
